ce90b59339f39a4c7ae7fe51075f1830a3498790
PHDorothy <97798826+PHDorothy@users.noreply.github.com>
2022-08-02 17:19:59 +0800

    Add files via upload

---
+++ change_suffix.py
@@ -0,0 +1,26 @@
+""" 
+批量改变文件后缀
+要遍历文件夹文件，提取文件后缀，并判断是否是需要的后缀，若不是则修改后缀。这里有个问题我要怎么修改才能放回去原文件？
+ listdir函数 os.listdir(path)  
+ path -- 需要列出的目录路径
+ 返回指定路径下的文件和文件夹列表
+splitext函数
+作用是分离文件名与扩展名，返回一个元组
+import os
+print (os.path.splitext('a_3.py'))---> ('a_3' '.py')
+路径还是不会???
+ """
+
+import os 
+
+
+path="D:\Code\test\"
+file_list= os.listdir(path)
+for file in file_list:
+    file_split = os.path.splitext(file)
+    if len(file_split)==2:
+        #这里的'txt'备替换
+        if file_split[-1]== 'txt':
+            #这里的'Word'替换结果
+            newfile =file_split[0]+'word'
+            os.rename(file,newfile)
