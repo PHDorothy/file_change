866d5af30b52d8e5a7a6788f10c7952e838afded
Jake <dorjake@163.com>
2022-08-07 20:19:43 +0800

    file manager plan

---
+++ file_manager.py
@@ -0,0 +1,13 @@
+"""
+
+完成一个文件管理器，需要支持以下命令
+
+file_manager modifyname <cur_name> <new_name> [-r|--recursion] [-re|--regular]
+
+file_manager findname <path> --name <file_name> [-r|--recursion] [-re|--regular]
+
+file_manager findcontent <path> --content <content> [-r|--recursion] [-re|--regular]
+
+file_manager replace <path> <cur_content> <new_content> [-r|--recursion] [-re|--regular]
+
+"""
